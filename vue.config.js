1cface386acd3dcfaa7101fedab90cd02dc9af07
ymsk.sky <ymsk.sky.95@gmail.com>
2019-12-18 21:30:19 +0900

    laravel_api_serverへtargetを変更

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ devServer: {
   port: 3000,
   proxy: {
     '/api': {
-      target: 'https://api.search.nicovideo.jp/api/v2/video/contents/search',
+      // target: 'https://api.search.nicovideo.jp/api/v2/video/contents/search',
+      target: 'http://localhost:8000',
       pathRewrite: {'^/api': ''}
     }
   }
